5188110675fec0220fbf7e56606c0dd39d62c0e2
aanish27 <aanish2710@gmail.com>
2025-02-19 19:43:44 +0530

    layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
-
 function App() {
   return (
-    <>
-      <div className='flex justify-center align-items-center bg-sky-950'>Is the template Working </div>
-    </>
-  )
+    <div className="flex justify-between items-center">
+      <div className="sidebar p-8 bg-red-400  h-[100vh] w-[15vw]">
+        <div> Instagram </div>
+        <div><span>Icon</span> Home</div>
+      </div>
+      <main>Posts & Stories</main>
+      <div>Right Sidebar</div>
+    </div>
+  );
 }
 
 export default App
+
