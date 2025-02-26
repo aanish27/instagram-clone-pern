18fc5b5d8ec7c944bebce1fe7bd43c65ed0ce141
aanish27 <aanish2710@gmail.com>
2025-02-26 20:01:58 +0530

    userValidator

---
+++ server/shared/validators/user.joi.validator.js
@@ -0,0 +1,34 @@
+const Joi = require("joi");
+
+const createUserValidationSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  username: Joi.string().min(6).max(20).required(),
+  bio: Joi.string().min(6).max(100),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .messages({ "string.pattern.base": `Phone number must have 10 digits.` })
+    .required(),
+  password: Joi.string().alphanum().min(8).max(30).required(),
+});
+
+const updateUserValidationSchema = Joi.object({
+  name: Joi.string().min(3),
+  username: Joi.string().min(6).max(20),
+  bio: Joi.string().min(6).max(100),
+  email: Joi.string().email(),
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .messages({ "string.pattern.base": `Phone number must have 10 digits.` }),
+  password: Joi.string().alphanum().min(8).max(30),
+});
+
+const getUserByIdValidationSchema = Joi.object({
+  id: Joi.number().positive(),
+});
+
+module.exports = {
+  createUserValidationSchema,
+  updateUserValidationSchema,
+  getUserByIdValidationSchema,
+};
