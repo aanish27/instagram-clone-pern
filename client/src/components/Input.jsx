ec600d102e2afeac9fe5fd56fd1a8939183ca4c1
aanish27 <aanish2710@gmail.com>
2025-03-18 00:50:42 +0530

    env file and default user

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,10 +1,11 @@
-function Input({type, placeholder, name , onChange}) {
+function Input({type, placeholder, name , onChange , value}) {
   return (
     <input
       type={type}
       placeholder={placeholder}
       className="mt-2 rounded-[2px] border-1 border-[#424242] bg-[#191919] p-1 placeholder:text-xs focus:outline-none"
       name={name}
+      value={value}
       onChange={onChange}
     />
   );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,12 +9,14 @@ import { useAuth } from "../provider/authProvider";
 import Cookies from "js-cookie";
 
 function Login() {
+  const env = import.meta.env.VITE_ENVIRONMENT;
+
   const { setToken } = useAuth();
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: (env == "local") ? "admin@gmail.com" : "",
+    password: (env == "local") ? "password" : "",
   });
 
   const handleSubmit = async (e) => {
