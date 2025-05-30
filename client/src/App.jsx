3a093ea0a3b89222be0a3555ac064af866b9b074
aanish27 <aanish2710@gmail.com>
2025-05-30 23:27:26 +0530

    react-query-devtools integrated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import AuthProvider from "./provider/authProvider";
 import Routes from "./routes";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <AuthProvider>
-      <ToastContainer />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer />
+        <Routes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </AuthProvider>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
